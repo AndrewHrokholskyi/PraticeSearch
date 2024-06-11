60de23e34e61deb551f0be60bd55e41ffad257c2
Andrew <anrrusha123@gmail.com>
2024-06-11 17:02:16 +0300

    feat: CreateProfile

---
+++ src/pages/Profile/components/CreateStudentProfile/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateStudentProfile = () => {
+  return (
+    <div>
+      <h1>Створення профілю</h1>
+    </div>
+  );
+};
+
+export default CreateStudentProfile;
